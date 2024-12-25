2aebcbf2a6822a66d44319ecf877afdd82d101fc
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-12-25 17:17:45 +0530

    understood the problem

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,1 +1,27 @@
-export default function Profile() {}
+"use client";
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function Profile() {
+  const [profile, setProfile] = useState("");
+
+  useEffect(() => {
+    const res = axios
+      .get("http://localhost:3000/api/profile", {
+        headers: {
+          authorization: localStorage.getItem("token"),
+        },
+      })
+      .then((res) => {
+        setProfile(res.data.avatarUrl);
+      });
+  }, []);
+
+  return <div>{profile}</div>;
+}
+
+// cant use the above approach because it initially sends a empty HTMl page
+// after some time it is send the request to profile
+// of course i am sending the header with it which reads the token from backend
+// but we lose the power of server side rendering coz the first response we only had an empty page
