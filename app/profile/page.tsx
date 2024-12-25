2aee88f857b65b8fdc61a90f008c23650ce84615
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-12-25 17:23:01 +0530

    now the first request is not the empty page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,27 +1,43 @@
-"use client";
+// "use client";
 
 import axios from "axios";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-export default function Profile() {
-  const [profile, setProfile] = useState("");
+// export default function Profile() {
+//   const [profile, setProfile] = useState("");
 
-  useEffect(() => {
-    const res = axios
-      .get("http://localhost:3000/api/profile", {
-        headers: {
-          authorization: localStorage.getItem("token"),
-        },
-      })
-      .then((res) => {
-        setProfile(res.data.avatarUrl);
-      });
-  }, []);
+//   useEffect(() => {
+//     const res = axios
+//       .get("http://localhost:3000/api/profile", {
+//         headers: {
+//           authorization: localStorage.getItem("token"),
+//         },
+//       })
+//       .then((res) => {
+//         setProfile(res.data.avatarUrl);
+//       });
+//   }, []);
 
-  return <div>{profile}</div>;
+//   return <div>{profile}</div>;
+// }
+
+// // cant use the above approach because it initially sends a empty HTMl page
+// // after some time it is send the request to profile
+// // of course i am sending the header with it which reads the token from backend
+// // but we lose the power of server side rendering coz the first response we only had an empty page
+// // the one very subtle problem not getting the user image rendered from the server
+
+// what should we do then we should use server components
+
+export default async function Profile() {
+  // const res = axios.get("http://localhost:3000/api/profile", {
+  //   headers: {
+  //     authorization: localStorage.getItem("token"),
+  //   },
+  // });
+
+  // const profilePicture = res.data.avatarUrl;
+  const profilePicture = "https://pic.png";
+
+  return <div>{profilePicture}</div>;
 }
-
-// cant use the above approach because it initially sends a empty HTMl page
-// after some time it is send the request to profile
-// of course i am sending the header with it which reads the token from backend
-// but we lose the power of server side rendering coz the first response we only had an empty page
